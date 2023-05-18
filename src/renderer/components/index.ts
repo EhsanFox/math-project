01b47d498aa542c33fd1048f4e971ddb9ef64e43
Bagher hosseini <bagherhosseini03@gmail.com>
2023-05-18 20:16:07 +0200

    export HomeBtn

--- src/renderer/components/index.ts
+++ src/renderer/components/index.ts
@@ -2,3 +2,4 @@ export { default as TitleBar } from './titlebar';
 export { default as Loading } from './loading';
 export { default as Variant } from './variant';
 export { default as NarrowButtons } from './button';
+export { default as HomeBtn } from './homeBtn/homeBtn';
